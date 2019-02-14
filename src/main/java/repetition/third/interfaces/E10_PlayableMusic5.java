cae6d76cf935c7c8b60a8ae2b3a9e8315cb339f8
osama <alsamancov@gmail.com>
2019-02-14 16:20:36 +0200

    "new classes in interfaces chapter"

---
+++ src/main/java/repetition/third/interfaces/E10_PlayableMusic5.java
@@ -0,0 +1,10 @@
+//: interfaces/E10_PlayableMusic5.java
+/****************** Exercise 10 ***********************************
+ * Add a Playable to modify Music5.java, and move tha play() declaration from Instrument to Playable.
+ * Include Playable in the implements list to add it to the derived classes.
+ * Change tune() so it takes a Playable instead of an Instrument.
+ ********************************************************************/
+package repetition.third.interfaces;
+
+public class E10_PlayableMusic5 {
+}
