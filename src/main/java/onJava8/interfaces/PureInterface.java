d2cf47d08524dcef47225e05283948066ad56f24
osama <alsamancov@gmail.com>
2019-02-19 17:20:38 +0200

    "new lessons"

---
+++ src/main/java/onJava8/interfaces/PureInterface.java
@@ -0,0 +1,10 @@
+// interfaces/PureInterface.java
+// Interface only looked like this before Java 8
+
+package onJava8.interfaces;
+
+public interface PureInterface {
+    int m1();
+    void m2();
+    double m3();
+}
